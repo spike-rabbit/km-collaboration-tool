feb8bfd181465160e309bf5a6faf05c6be053181
LFGWasTaken <leon.g@paderborn.com>
2017-01-07 18:49:01 +0100

    Removed error.

---
+++ kmct_server/api/xcc-usage.ts
@@ -0,0 +1,24 @@
+/**
+ * Created by L on 05.01.2017.
+ */
+import * as express from "express";
+import {ProtectedRequest} from "../authentication-manager";
+import {database} from "../database-manager";
+import * as uuid from "uuid";
+let router = express.Router();
+router.get('/ausbildungsnachweise', getAusbildungsnachweise);
+
+function getAusbildungsnachweise(req: ProtectedRequest, res: express.Response){
+    let where: {[key: string]: any} = {};
+    where['classId'] = req.user.classId;
+    database.ausbildungsnachweise.findAll({
+        where: where, attributes: []
+    }).then(nachweis => res.send({
+        ausbildungsnachweise: nachweis, classId: req.user.classId
+    }), reason => {
+        //TODO log better
+        //TODO send error to client
+        console.log(reason);
+    })
+}
+
