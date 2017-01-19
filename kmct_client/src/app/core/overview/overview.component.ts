5d3dc5ce78ea34d8324f34b85416264902122aa0
Jil123 <jil.hoffmeister@education-siemens.com>
2017-01-19 16:18:30 +0100

    Nav leiste + "Firmen verwalten" manage-companies

--- kmct_client/src/app/core/overview/overview.component.ts
+++ kmct_client/src/app/core/overview/overview.component.ts
@@ -8,7 +8,7 @@ import {Options} from "fullcalendar";
   selector: 'app-overview',
   template: `
 <h3>Herzlich willkommen im KM Collaboration Tool!</h3>
-  <a routerLink="xcc" routerLinkActive="active">Hier geht's zu den Ausbildungsnachweisen</a>
+  <a routerLink="xcc" routerLinkActive="active" i18n>Hier geht's zu den Ausbildungsnachweisen</a>
   <br>
   <a routerLink="xcm" routerLinkActive="active">Hier geht's zum Exchange Market</a>
   <br>
